5eb37123ff281c30fd3d763de79d5b7016238978
SimPlePig <rlatjrfo5@gmail.com>
2018-01-23 23:43:47 +0900

    맵GUI생성&랜덤장애물생성

---
+++ 5W1H/Assets/SimPlePig/Editor/MapEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor (typeof (MapMgr))]
+public class MapEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        MapMgr map = target as MapMgr;
+
+        map.GenerateMap();
+    }
+}
