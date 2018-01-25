ea562a80d79ed1a3bc20761d06a5cb209d254e6b
SimPlePig <rlatjrfo5@gmail.com>
2018-01-26 00:12:09 +0900

    고립지역이 없는 랜덤맵 에디터 구현

--- 5W1H/Assets/SimPlePig/Scripts/MapMgr.cs
+++ 5W1H/Assets/SimPlePig/Scripts/MapMgr.cs
@@ -11,12 +11,17 @@ public class MapMgr : MonoBehaviour {
 
     [Range(0, 1)]
     public float outlinePrecent;
+    [Range(0, 1)]
+    public float obstaclePercent;
 
     List<Coord> allTileCoords;
     Queue<Coord> shuffledTileCoords;
 
 
-    public int seed = 10;
+    public int seed = 10;// 장애물랜덤값
+    Coord mapCentre;//맵의 중앙
+
+
 
     private void Start()
     {
@@ -38,7 +43,11 @@ public class MapMgr : MonoBehaviour {
 
         shuffledTileCoords = new Queue<Coord> (Utility.ShuffleArray(allTileCoords.ToArray(),seed));
 
-                string holderName = " Generated Map";
+        mapCentre = new Coord((int)mapSize.x / 2, (int)mapSize.y / 2);
+
+
+
+        string holderName = " Generated Map";
 
         if (transform.Find(holderName)) 
         {
@@ -60,15 +69,70 @@ public class MapMgr : MonoBehaviour {
             }
         }
 
-        int obstacleCount = 10;
-        for(int i=0;i<obstacleCount;i++)
+        bool[,] obstacleMap = new bool[(int)mapSize.x,(int)mapSize.y];
+
+
+
+        int obstacleCount = (int)(mapSize.x*mapSize.y*obstaclePercent); //장애물의 갯수를 설정하는 칸
+        int currentObstacleCount = 0;
+
+        for (int i=0;i<obstacleCount;i++)
         {
             Coord randomCoord = GetRandomCoord();
-            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
-            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up*0.5f, Quaternion.identity) as Transform;
-            newObstacle.parent = mapHolder;
+            obstacleMap[randomCoord.x, randomCoord.y] = true;
+            currentObstacleCount++;
+            if (randomCoord != mapCentre && MapIsFullyAccessible(obstacleMap, currentObstacleCount))
+            {
+                Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+                Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition + Vector3.up * 0.5f, Quaternion.identity) as Transform;
+                newObstacle.parent = mapHolder;
+            }
+            else
+            {
+                obstacleMap[randomCoord.x, randomCoord.y] = false;
+                currentObstacleCount--;
+            }
+            
         }
 
+    }
+
+    bool MapIsFullyAccessible(bool[,] obstacleMap ,int currentObstacleCount)//장애물을 검색하는 메소드(플러드필 알고리즘)
+    {
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(mapCentre);
+        mapFlags[mapCentre.x, mapCentre.y] = true;
+
+        int accessibleTileCount = 1;
+
+        while (queue.Count > 0)
+        {
+            Coord tile = queue.Dequeue();
+
+            for(int x = -1; x <= 1; x++)
+            {
+                for(int y = -1; y <= 1; y++)
+                {
+                    int neighbourX = tile.x + x;
+                    int neighbourY = tile.y + y;
+                    if(x==0||y==0)
+                    {
+                        if(neighbourX>=0 && neighbourX < obstacleMap.GetLength(0) && neighbourY >= 0 && neighbourY < obstacleMap.GetLength(1))
+                        {
+                            if (!mapFlags[neighbourX, neighbourY] && !obstacleMap[neighbourX, neighbourY])
+                            {
+                                mapFlags[neighbourX, neighbourY] = true;
+                                queue.Enqueue(new Coord(neighbourX, neighbourY));
+                                accessibleTileCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int targetAccessibleTileCount = (int)(mapSize.x * mapSize.y - currentObstacleCount);
+        return targetAccessibleTileCount == accessibleTileCount;
     }
 
 
@@ -95,6 +159,15 @@ public class MapMgr : MonoBehaviour {
         {
             x = _x;
             y = _y;
+        }
+
+        public static bool operator ==(Coord c1, Coord c2)
+        {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1 == c2);
         }
     }
 }
